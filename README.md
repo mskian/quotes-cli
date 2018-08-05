5dea7e06f4f927af827b987b3959f6b7aa86befa
mskian <veerfun23@gmail.com>
2018-08-05 16:00:27 +0530

    First Update

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# quotes-cli
-A Node CLI to display random quotes in Terminal
+# Quotes CLI
+
+> A Node CLI to display random quotes in Terminal
+
+## Install
+
+```
+```
+
+## Usage
+
+- Just run the Below Commnad
+
+```
+```
+
+## API
+
+Visit - https://quotesapi.mskian.com/
+
+## License
+
+MIT
---
+++ app.js
@@ -0,0 +1,38 @@
+#!/usr/bin/env node
+
+const ora = require("ora")
+const fetch = require("node-fetch")
+const chalk = require("chalk")
+const emoji = require('node-emoji');
+
+async function fetchquotes(){
+    const spinner = ora("Fetching Quotes").start()
+  
+    try {
+      await new Promise(resolve => setTimeout(resolve, 1000))
+      res = await fetch("https://quotesapi.mskian.com/all")
+      if (res.ok) {
+        json = await res.json()
+        spinner.stop()
+        printContent(json)
+      } else {
+        json = await res.json()
+        spinner.stop()
+        console.log(`title: ${json.errors.title}
+              detail: ${json.errors.detail}`)
+      }
+    } catch (err) {
+      spinner.stop()
+  
+      console.error(err)
+    }
+  }
+
+  fetchquotes()
+
+  const printContent = json => {
+    console.log()
+    var random = json.quoteswritten[Math.floor(Math.random() * json.quoteswritten.length)];
+    console.log(emoji.get('unicorn_face'), chalk.bgYellowBright.bold.black(random.quotes));
+    console.log()
+  }
