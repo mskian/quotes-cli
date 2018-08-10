21169dbd21974a2a510abe97aca44e8c6f6d6436
mskian <veerfun23@gmail.com>
2018-08-10 18:26:37 +0530

    Minor Bug Fix

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+    "env": {
+        "es6": true,
+        "node": true,
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 2018
+    },
+    "globals": {
+        "json": true
+    },
+    "rules": {
+        "no-console": 0,
+        "indent": [
+            "error",
+            "tab",
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
+};
--- app.js
+++ app.js
@@ -1,38 +1,38 @@
 #!/usr/bin/env node
 
-const ora = require("ora");
-const fetch = require("node-fetch");
-const chalk = require("chalk");
+const ora = require('ora');
+const fetch = require('node-fetch');
+const chalk = require('chalk');
 const emoji = require('node-emoji');
 
 async function fetchquotes(){
-    const spinner = ora("Fetching Quotes").start()
+	const spinner = ora('Fetching Quotes').start();
   
-    try {
-      await new Promise(resolve => setTimeout(resolve, 1000))
-      res = await fetch("https://quotesapi.mskian.com/all")
-      if (res.ok) {
-        json = await res.json()
-        spinner.stop()
-        printContent(json)
-      } else {
-        json = await res.json()
-        spinner.stop()
-        console.log(`title: ${json.errors.title}
-              detail: ${json.errors.detail}`)
-      }
-    } catch (err) {
-      spinner.stop()
+	try {
+		await new Promise(resolve => setTimeout(resolve, 1000));
+		const response = await fetch('https://quotesapi.mskian.com/all');
+		if (response.ok) {
+			const json = await response.json();
+			spinner.stop();
+			printContent(json);
+		} else {
+			json = await response.json();
+			spinner.stop();
+			console.log(`title: ${json.errors.title}
+              detail: ${json.errors.detail}`);
+		}
+	} catch (err) {
+		spinner.stop();
   
-      console.error(err)
-    }
-  }
+		console.error(err);
+	}
+}
 
-  fetchquotes();
+fetchquotes();
 
-  const printContent = json => {
-    console.log();
-    var random = json.quoteswritten[Math.floor(Math.random() * json.quoteswritten.length)];
-    console.log(emoji.get('unicorn_face'), chalk.bgYellowBright.bold.black(random.quotes));
-    console.log();
-  }
+const printContent = json => {
+	console.log();
+	var random = json.quoteswritten[Math.floor(Math.random() * json.quoteswritten.length)];
+	console.log(emoji.get('point_right'), chalk.bgYellowBright.bold.black(random.quotes));
+	console.log();
+};
