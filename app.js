aaea43b5686bb190c4f46eebaaddde2b0e5160b1
mskian <veerfun23@gmail.com>
2018-08-28 16:31:19 +0530

    Update API

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ async function fetchquotes(){
   
 	try {
 		await new Promise(resolve => setTimeout(resolve, 1000));
-		const response = await fetch('https://quotesapi.mskian.com/all');
+		const response = await fetch('https://mskquotes.herokuapp.com/all');
 		if (response.ok) {
 			const json = await response.json();
 			spinner.stop();
